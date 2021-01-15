101898dbad126eeca25fc9422c87e4911a03e112
Paul Varner <fuzzyray@gmail.com>
2021-01-14 18:04:39 -0600

    Implement /api/translate route

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,35 @@
 const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanToBritishTitles = require('./american-to-british-titles.js');
+const britishOnly = require('./british-only.js');
 
 class Translator {
+  constructor(text = null, locale = null) {
+    this.text = text;
+    this.locale = locale;
+    this.translation = null;
+  }
 
+  setText(text) {
+    if (!!text) {
+      this.text = text;
+    }
+  }
+
+  setLocale(locale) {
+    if (!!locale) {
+      this.locale = locale;
+    }
+  }
+
+  translate() {
+    this.translation = 'not implemented'
+  }
+
+  toString() {
+    return JSON.stringify(this)
+  }
 }
+
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,31 @@
 
 const Translator = require('../components/translator.js');
 
-module.exports = function (app) {
-  
+module.exports = function(app) {
+
   const translator = new Translator();
+  const valid_locales = ['american-to-british', 'british-to-american'];
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      if (req.body.hasOwnProperty('text') &&
+        req.body.hasOwnProperty('locale')) {
+        if (req.body.text === '') {
+          res.json({error: 'No text to translate'});
+        } else if (!valid_locales.includes(req.body.locale)) {
+          res.json({error: 'Invalid value for locale field'});
+        } else {
+          translator.setText(req.body.text);
+          translator.setLocale(req.body.locale);
+          translator.translate();
+          console.log(translator.toString());
+          res.json({
+            'text': translator.text,
+            'translation': translator.translation,
+          });
+        }
+      } else {
+        res.json({'error': 'Required field(s) missing'});
+      }
     });
 };
