7bf9a12ccc1f9066c4179aff75885f190bffdf8b
Paul Varner <fuzzyray@gmail.com>
2021-01-19 12:56:40 -0600

    Implement translation of words

--- components/translator.js
+++ components/translator.js
@@ -3,11 +3,31 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require('./american-to-british-titles.js');
 const britishOnly = require('./british-only.js');
 
+let americanToBritish = [];
+let britishToAmerican = [];
+
+const createDictionary = (dictObj) => {
+  const multiWord = [];
+  const singleWord = [];
+  Object.keys(dictObj).forEach((key) => {
+    const wordObj = {};
+    wordObj[key] = dictObj[key];
+    if (key.split(/\s/).length > 1) {
+      multiWord.push(wordObj);
+    } else {
+      singleWord.push(wordObj);
+    }
+  });
+  return [].concat(multiWord, singleWord);
+};
+
 class Translator {
   constructor(text = null, locale = null) {
     this.text = text;
     this.locale = locale;
     this.translation = null;
+    americanToBritish = createDictionary(americanOnly)
+    britishToAmerican = createDictionary(britishOnly)
   }
 
   setText(text) {
@@ -23,13 +43,25 @@ class Translator {
   }
 
   translate() {
-    this.translation = 'not implemented'
+    let translatedText = this.text;
+    let myDict;
+    translatedText = this.text;
+    if (this.locale === 'american-to-british') {
+      myDict = americanToBritish;
+    } else if (this.locale === 'british-to-american') {
+      myDict = britishToAmerican;
+    }
+    myDict.forEach((obj) => {
+      const key = Object.keys(obj).join('')
+      const re = new RegExp(`(\\s+|^)${key}(\\s+|$)`, 'ig');
+      translatedText = translatedText.replace(re, `$1${obj[key]}$2`);
+    });
+    this.translation = translatedText;
   }
 
   toString() {
-    return JSON.stringify(this)
+    return JSON.stringify(this);
   }
 }
-
 
 module.exports = Translator;
