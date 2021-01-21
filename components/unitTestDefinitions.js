bed8360e9ac53dac7d952dd579deb3d9095a660c
Paul Varner <fuzzyray@gmail.com>
2021-01-21 10:33:42 -0600

    Implement initial unit test definitions

---
+++ components/unitTestDefinitions.js
@@ -0,0 +1,170 @@
+module.exports = [
+  // Translate Mangoes are my favorite fruit. to British English
+  {
+    text: 'Mangoes are my favorite fruit.',
+    locale: 'american-to-british',
+    translation:
+        'Mangoes are my <span class="highlight">favourite</span> fruit.',
+  },
+  // Translate I ate yogurt for breakfast. to British English
+  {
+    text: 'I ate yogurt for breakfast.',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+  // Translate We had a party at my friend's condo. to British English
+  {
+    text: 'We had a party at my friend\'s condo.',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+  // Translate Can you toss this in the trashcan for me? to British English
+  {
+    text: 'Can you toss this in the trashcan for me?',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+  // Translate The parking lot was full. to British English
+  {
+    text: 'The parking lot was full.',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+  // Translate Like a high tech Rube Goldberg machine. to British English
+  {
+    text: 'Like a high tech Rube Goldberg machine.',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+  // Translate To play hooky means to skip class or work. to British English
+  {
+    text: 'To play hooky means to skip class or work.',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+  // Translate No Mr. Bond, I expect you to die. to British English
+  {
+    text: 'No Mr. Bond, I expect you to die.',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+  // Translate Dr. Grosh will see you now. to British English
+  {
+    text: 'Dr. Grosh will see you now.',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+  // Translate Lunch is at 12:15 today. to British English
+  {
+    text: ' Lunch is at 12:15 today.',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+  // Translate We watched the footie match for a while. to American English
+  {
+    text: 'We watched the footie match for a while.',
+    locale: 'british-to-american',
+    translation:
+        'Not implemented',
+  },
+  // Translate Paracetamol takes up to an hour to work. to American English
+  {
+    text: 'Paracetamol takes up to an hour to work.',
+    locale: 'british-to-american',
+    translation:
+        'Not implemented',
+  },
+  // Translate First, caramelise the onions. to American English
+  {
+    text: 'First, caramelise the onions.',
+    locale: 'british-to-american',
+    translation:
+        'Not implemented',
+  },
+  // Translate I spent the bank holiday at the funfair. to American English
+  {
+    text: 'I spent the bank holiday at the funfair.',
+    locale: 'british-to-american',
+    translation:
+        'Not implemented',
+  },
+  // Translate I had a bicky then went to the chippy. to American English
+  {
+    text: 'I had a bicky then went to the chippy.',
+    locale: 'british-to-american',
+    translation:
+        'Not implemented',
+  },
+  // Translate I've just got bits and bobs in my bum bag. to American English
+  {
+    text: 'I\'ve just got bits and bobs in my bum bag.',
+    locale: 'british-to-american',
+    translation:
+        'Not implemented',
+  },
+  // Translate The car boot sale at Boxted Airfield was called off. to American English
+  {
+    text: 'The car boot sale at Boxted Airfield was called off.',
+    locale: 'british-to-american',
+    translation:
+        'Not implemented',
+  },
+  // Translate Have you met Mrs Kalyani? to American English
+  {
+    text: 'Have you met Mrs Kalyani?',
+    locale: 'british-to-american',
+    translation:
+        'Not implemented',
+  },
+  // Translate Prof Joyner of King's College, London. to American English
+  {
+    text: 'Prof Joyner of King\'s College, London.',
+    locale: 'british-to-american',
+    translation:
+        'Not implemented',
+  },
+  // Translate Tea time is usually around 4 or 4.30. to American English
+  {
+    text: 'Tea time is usually around 4 or 4.30.',
+    locale: 'british-to-american',
+    translation:
+        'Not implemented',
+  },
+  // Highlight translation in Mangoes are my favorite fruit.
+  {
+    text: 'Mangoes are my favorite fruit.',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+  // Highlight translation in I ate yogurt for breakfast.
+  {
+    text: 'I ate yogurt for breakfast.',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+  // Highlight translation in We watched the footie match for a while.
+  {
+    text: 'We watched the footie match for a while.',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+  // Highlight translation in Paracetamol takes up to an hour to work.
+  {
+    text: 'Paracetamol takes up to an hour to work.',
+    locale: 'american-to-british',
+    translation:
+        'Not implemented',
+  },
+];
